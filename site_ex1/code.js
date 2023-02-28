9db1b4eed7569abfbc9dda36623e05b717d3b503
Santozzz <107252394+Santozzz@users.noreply.github.com>
2023-02-28 12:40:22 -0300

    Add files via upload

---
+++ site_ex1/code.js
@@ -0,0 +1,15 @@
+function troca1(){
+    window.location.href = "index1.html"
+}
+
+function troca2(){
+    window.location.href = "index2.html"
+}
+
+function troca3(){
+    window.location.href = "index3.html"
+}
+
+function troca4(){
+    window.location.href = "index4.html"
+}
